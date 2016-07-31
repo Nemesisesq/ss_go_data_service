cbf9267fbe963dd5d6708d320ca4d51c1d0e2905
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-07-30 20:58:55 -0400

    default value for port

--- server.go
+++ server.go
@@ -5,14 +5,21 @@ import (
     "net/http"
     "fmt"
     "log"
+    "os"
 )
 
 
 func main () {
+    port := os.Getenv("PORT")
+    if port == "" {
+    port = "8080"
+    }
+
+
     r := mux.NewRouter()
     r.HandleFunc("/", index).Methods("GET")
     //http.Handle("/", r)
-    log.Fatal(http.ListenAndServe(":8080", r))
+    log.Fatal(http.ListenAndServe(":" + port, r))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
