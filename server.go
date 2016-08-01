b8717dfc04b30a5807ee8f27e5abf87990b4666a
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-08-01 11:32:52 -0400

    fixed variables for heroku

--- server.go
+++ server.go
@@ -6,22 +6,116 @@ import (
     "fmt"
     "log"
     "os"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+    //"encoding/json"
+    "encoding/json"
 )
 
-
-func main () {
+func main() {
     port := os.Getenv("PORT")
     if port == "" {
-    port = "8080"
+        port = "8080"
     }
-
 
     r := mux.NewRouter()
     r.HandleFunc("/", index).Methods("GET")
-    //http.Handle("/", r)
+    r.HandleFunc("/test/", testHandler).Methods("POST")
+    r.HandleFunc("/test/{email}", testHandler).Methods("GET")
     log.Fatal(http.ListenAndServe(":" + port, r))
+
+    fmt.Println("application is running @ http://localhost:8080")
+
+}
+
+
+//// DATABASE Mongo connection
+
+
+type EDRecord struct {
+    Email    string `json: "email"`
+    DeviceID []string `json : "deviceId"`
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "hello world")
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+    port := os.Getenv("DATABASE_URL")
+
+    session, err := mgo.Dial(port)
+
+    if err != nil {
+        panic(err)
+    }
+
+    defer session.Close()
+
+    session.SetMode(mgo.Monotonic, true)
+    c := session.DB("test").C("ed_records")
+
+    if r.Method == "GET" {
+
+        vars := mux.Vars(r)
+
+        email := vars["email"]
+
+        //m := r.URL.Path[len("/test/"):]
+
+        //fmt.Fprint(w, m)
+
+        //fmt.Println(m)
+
+        //if m == nil {
+        //    http.NotFound(w, r)
+        //}
+
+        result := &EDRecord{}
+        err = c.Find(bson.M{"email": email}).One(result)
+
+        if err != nil {
+            //log.Fatal(err)
+            http.NotFound(w, r)
+        }
+
+        res, err := json.Marshal(result)
+
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            //return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.Write(res)
+
+    }
+
+    if r.Method == "POST" {
+
+        decoder := json.NewDecoder(r.Body)
+
+        fmt.Println(r.Body)
+
+        var t EDRecord
+
+        err = decoder.Decode(&t)
+
+        if err != nil {
+            log.Fatal(err)
+            fmt.Print(err)
+        }
+
+        fmt.Println(t)
+
+        err = c.Insert(t)
+
+        if err != nil {
+            log.Fatal(err)
+            fmt.Print(err)
+        }
+
+        fmt.Fprint(w, "OKAY!")
+    }
+
 }
