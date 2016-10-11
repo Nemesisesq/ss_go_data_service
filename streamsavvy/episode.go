d461d5eb94eef2a77b42539c607c7dda4c5b7818
Nem <nemesisesq@gmail.com>
2016-10-11 15:34:22 -0700

    grabbing episode links

--- streamsavvy/episode.go
+++ streamsavvy/episode.go
@@ -11,33 +11,32 @@ import (
 	com "github.com/nemesisesq/ss_data_service/common"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-
-
 )
 
 type Episode struct {
-	Id               int      `json:"id"`
-	Tvdb             int      `json:"tvdb"`
-	ContentType      string   `json:"content_type"`
-	IsShadow         int      `json:"is_shadow"`
-	AlternateTvdb    []string `json:"alternate_tvdb"`
-	ImdbId           string   `json:"imdb_id"`
-	SeasonNumber     int      `json:"season_number"`
-	EpisodeNumber    int      `json:"episode_number"`
-	ShowId           int      `json:"show_id"`
-	Themoviedb       int      `json:"themoviedb"`
-	Special          int      `json:"special"`
-	FirstAired       string   `json:"first_aired"`
-	Title            string   `json:"title"`
-	OriginalTitle    string   `json:"original_title"`
-	AlternateTitles  []string `json:"alternate_titles"`
-	Overview         string   `json:"overview"`
-	Duration         int      `json:"duration"`
-	ProductionCode   string   `json:"production_code"`
-	Thumbnail208X117 string   `json:"thumbnail_208x117"`
-	Thumbnail304X171 string   `json:"thumbnail_304x171"`
-	Thumbnail400X225 string   `json:"thumbnail_400x225"`
-	Thumbnail608X342 string   `json:"thumbnail_608x342"`
+	Id                     int      `json:"id"`
+	Tvdb                   int      `json:"tvdb"`
+	ContentType            string   `json:"content_type"`
+	IsShadow               int      `json:"is_shadow"`
+	AlternateTvdb          []string `json:"alternate_tvdb"`
+	ImdbId                 string   `json:"imdb_id"`
+	SeasonNumber           int      `json:"season_number"`
+	EpisodeNumber          int      `json:"episode_number"`
+	ShowId                 int      `json:"show_id"`
+	Themoviedb             int      `json:"themoviedb"`
+	Special                int      `json:"special"`
+	FirstAired             string   `json:"first_aired"`
+	Title                  string   `json:"title"`
+	OriginalTitle          string   `json:"original_title"`
+	AlternateTitles        []string `json:"alternate_titles"`
+	Overview               string   `json:"overview"`
+	Duration               int      `json:"duration"`
+	ProductionCode         string   `json:"production_code"`
+	Thumbnail208X117       string   `json:"thumbnail_208x117"`
+	Thumbnail304X171       string   `json:"thumbnail_304x171"`
+	Thumbnail400X225       string   `json:"thumbnail_400x225"`
+	Thumbnail608X342       string   `json:"thumbnail_608x342"`
+	SubscriptionWebSources interface{} `json:"subscription_web_sources"`
 }
 type GuideBoxEpisodes struct {
 	GuideboxId    string    `bson:"guidebox_id"`
@@ -45,8 +44,6 @@ type GuideBoxEpisodes struct {
 	TotalResults  int   `json:"total_results" bson:"total_results"`
 	TotalReturned int    `json:"total_returned" bson:"total_returned"`
 }
-
-
 
 func GetEpisodes(w http.ResponseWriter, r *http.Request) {
 
@@ -63,7 +60,7 @@ func GetEpisodes(w http.ResponseWriter, r *http.Request) {
 
 	com.Check(err)
 
-	if count > 0 {
+	if count <  0 {
 		query.One(&epi)
 	} else {
 
