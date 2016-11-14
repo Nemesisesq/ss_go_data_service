8885159e2559f4a37c16fd3b64447e3594687818
Nem <nemesisesq@gmail.com>
2016-11-14 12:03:27 -0500

    filtered out showtime

--- streamsavvy/episode.go
+++ streamsavvy/episode.go
@@ -14,36 +14,36 @@ import (
 )
 
 type Episode struct {
-	Id                         int         `json:"id"`
-	Tvdb                       int         `json:"tvdb"`
+	Id                         interface{}         `json:"id"`
+	Tvdb                       interface{}         `json:"tvdb"`
 	ContentType                string      `json:"content_type"`
-	IsShadow                   int         `json:"is_shadow"`
+	IsShadow                   interface{}         `json:"is_shadow"`
 	AlternateTvdb              []string    `json:"alternate_tvdb"`
 	ImdbId                     string      `json:"imdb_id"`
-	SeasonNumber               int         `json:"season_number"`
-	EpisodeNumber              int         `json:"episode_number"`
-	ShowId                     int         `json:"show_id"`
-	Themoviedb                 int         `json:"themoviedb"`
-	Special                    int         `json:"special"`
+	SeasonNumber               interface{}         `json:"season_number"`
+	EpisodeNumber              interface{}         `json:"episode_number"`
+	ShowId                     interface{}         `json:"show_id"`
+	Themoviedb                 interface{}         `json:"themoviedb"`
+	Special                    interface{}         `json:"special"`
 	FirstAired                 string      `json:"first_aired"`
 	Title                      string      `json:"title"`
 	OriginalTitle              string      `json:"original_title"`
 	AlternateTitles            []string    `json:"alternate_titles"`
 	Overview                   string      `json:"overview"`
-	Duration                   int         `json:"duration"`
+	Duration                   interface{}         `json:"duration"`
 	ProductionCode             string      `json:"production_code"`
 	Thumbnail208X117           string      `json:"thumbnail_208x117"`
 	Thumbnail304X171           string      `json:"thumbnail_304x171"`
 	Thumbnail400X225           string      `json:"thumbnail_400x225"`
 	Thumbnail608X342           string      `json:"thumbnail_608x342"`
-	FreeWebSources             interface{} `json:"free_web_sources"`
-	FreeIosSources             interface{} `json:"free_ios_sources"`
-	SubscriptionWebSources     interface{} `json:"subscription_web_sources"`
-	SubscriptionIosSources     interface{} `json:"subscription_ios_sources"`
-	SubscriptionAndroidSources interface{} `json:"subscription_android_sources"`
-	PurchaseWebSources         interface{} `json:"purchase_web_sources"`
-	PurchaseIosSources         interface{} `json:"purchase_ios_sources"`
-	PurchaseAndroidSources     interface{} `json:"purchase_android_sources"`
+	FreeWebSources             []map[string]interface{} `json:"free_web_sources"`
+	FreeIosSources             []map[string]interface{} `json:"free_ios_sources"`
+	SubscriptionWebSources     []map[string]interface{} `json:"subscription_web_sources"`
+	SubscriptionIosSources     []map[string]interface{} `json:"subscription_ios_sources"`
+	SubscriptionAndroidSources []map[string]interface{} `json:"subscription_android_sources"`
+	PurchaseWebSources         []map[string]interface{} `json:"purchase_web_sources"`
+	PurchaseIosSources         []map[string]interface{} `json:"purchase_ios_sources"`
+	PurchaseAndroidSources     []map[string]interface{} `json:"purchase_android_sources"`
 }
 type GuideBoxEpisodes struct {
 	GuideboxId    string        `bson:"guidebox_id"`
@@ -57,7 +57,7 @@ func init() {
 }
 
 func GetEpisodes(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
 	guideboxId := r.URL.Query().Get("guidebox_id")
 
 	epi := &GuideBoxEpisodes{}
@@ -67,8 +67,8 @@ func GetEpisodes(w http.ResponseWriter, r *http.Request) {
 	val, err := client.Get(guideboxId).Result()
 	ttl, err := client.TTL(guideboxId).Result()
 
-	log.Info(fmt.Sprintf("the redis error is %v", err))
-	log.Info(fmt.Sprintf("the value is %v", val))
+	//log.Info(fmt.Sprintf("the redis error is %v", err))
+	//log.Info(fmt.Sprintf("the value is %v", val))
 
 	if err == redis.Nil || len(val) == 0 {
 
@@ -76,14 +76,15 @@ func GetEpisodes(w http.ResponseWriter, r *http.Request) {
 
 		episode_list, total_results := epi.GetAllEpisodes(guideboxId)
 
+
 		epi.CacheEpisode(total_results, episode_list, guideboxId, *client)
 
 		epi.Results = episode_list
-		log.Info(fmt.Sprintf("this is the value of epi %v", epi))
+		//log.Info(fmt.Sprintf("this is the value of epi %v", epi))
 
 	} else {
 		log.Info("checking TTL", reflect.TypeOf(ttl))
-		if ttl < time.Hour*12 {
+		if ttl < time.Hour * 12 {
 			log.Info(fmt.Sprintf("refreshing %v", guideboxId))
 			go epi.RefreshEpisodes(guideboxId, *client)
 		}
@@ -92,15 +93,55 @@ func GetEpisodes(w http.ResponseWriter, r *http.Request) {
 		json.Unmarshal([]byte(val), &epi)
 	}
 
+	epi.Results = CleanUpDeepLinks(epi.Results)
+
 	json.NewEncoder(w).Encode(epi)
 
 }
 
+func CleanUpDeepLinks(epi_list []interface{}) []interface{} {
+	//res := []Episode{}
+
+	for indx, val := range epi_list {
+		x_epi := &Episode{}
+
+		the_json, _ := json.Marshal(val)
+		err := json.Unmarshal(the_json, &x_epi)
+		com.Check(err)
+		for indx, val := range x_epi.SubscriptionIosSources {
+			if val["source"].(string) == "hulu_with_showtime" {
+				log.WithField("length of sources before", len(x_epi.SubscriptionIosSources)).Info()
+				x_epi.SubscriptionIosSources = append(x_epi.SubscriptionIosSources[:indx], x_epi.SubscriptionIosSources[indx + 1:]...)
+				log.WithField("length of sources after", len(x_epi.SubscriptionIosSources)).Info()
+			}
+
+		}
+
+		the_json, _ = json.Marshal(x_epi)
+		err = json.Unmarshal(the_json, &val)
+
+		if indx == 0{
+
+			pretty, _ := json.MarshalIndent(val,"", "\t")
+			fmt.Println(string(pretty[:]))
+		}
+		com.Check(err)
+
+		epi_list[indx] = val
+
+
+	}
+
+
+	return epi_list
+}
+
 func (epi GuideBoxEpisodes) CacheEpisode(total_results int, episode_list []interface{}, guideboxId string, client redis.Client) {
-	print(total_results)
+	//print(total_results)
 	epi.Results = episode_list
 
-	log.Info(fmt.Sprintf("length of episode list %v", len(episode_list)))
+	//log.Info(fmt.Sprintf("length of episode list %v", len(episode_list)))
+
 
 	epi.GuideboxId = guideboxId
 	epi.TotalResults = total_results
