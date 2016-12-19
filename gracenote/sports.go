e490a727fc1d3a96894f0fc1d55cb75c5adefb4b
Nem <nemesisesq@gmail.com>
2016-12-19 16:43:50 -0500

    Go cypheer added and adding relationships

--- gracenote/sports.go
+++ gracenote/sports.go
@@ -1,25 +1,25 @@
 package gracenote
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 	"github.com/nemesisesq/ss_data_service/common"
 	"gopkg.in/square/go-jose.v1/json"
-	bolt"github.com/johnnadratowski/golang-neo4j-bolt-driver"
-	"os"
-	"github.com/Sirupsen/logrus"
-	"strings"
 )
 
 type Organization struct {
-	OrganizationId   string `json:"organizationId" bson:"organization_id"`
-	OrganizationName string `json:"organizationName" bson:"organizationName"`
+	OrganizationId   string            `json:"organizationId" bson:"organization_id"`
+	OrganizationName string            `json:"organizationName" bson:"organizationName"`
 	PreferredImage   map[string]string `json:"preferredImage" bson:"preferredImage"`
 }
 
 type Sport struct {
-	SportsId      string `json:"sportsId" bson:"sportsId"`
-	SportsName    string `json:"sportsName" bson:"sportsName"`
+	SportsId      string         `json:"sportsId" bson:"sportsId"`
+	SportsName    string         `json:"sportsName" bson:"sportsName"`
 	Organizations []Organization `json:"organizations" bson:"organizations"`
 }
 
@@ -36,33 +36,39 @@ func (sl SportsList) SaveSportsList() {
 
 	common.Check(err)
 
-	for _, val := range sl {
-		logrus.Info(val.SportsName, " sports name")
-		rmvSpace := strings.NewReplacer(" ", "", ".", "_", "'", "", "-", "", "/", "")
-		sport_node_name := rmvSpace.Replace(val.SportsName)
-		cypher_query := fmt.Sprintf(`CREATE (%v:Sport {sport_name:{sport_name}, gracenote_sport_id:{sports_id}})`, sport_node_name)
 
-		logrus.Info(sport_node_name, val.SportsName, val.SportsId)
-		params := map[string]interface{}{"sport_node_name": sport_node_name, "sport_name": val.SportsName, "sports_id": val.SportsId}
+
+	for _, val := range sl {
+		//rmvSpace := strings.NewReplacer(" ", "", ".", "_", "'", "", "-", "", "/", "")
+		//sport_node_name := rmvSpace.Replace(val.SportsName)
+		cypher_query := `CREATE (n:Sport {sport_name:{sport_name}, gracenote_sport_id:{sports_id}})`
+
+		params := map[string]interface{}{"sport_name": val.SportsName, "sports_id": val.SportsId}
 
 		ProcessCypher(conn, cypher_query, params)
 
 		for _, org := range val.Organizations {
 
-			org_node_name := rmvSpace.Replace(org.OrganizationName)
+			//org_node_name := rmvSpace.Replace(org.OrganizationName)
 
-			cypher_query := fmt.Sprintf(`CREATE (%v:Organization {organization:{org_name}, gracenote_organization_id:{org_id}})`, org_node_name)
-			params := map[string]interface{}{"sport_node_name": sport_node_name, "org_node_name": org_node_name, "org_name": org.OrganizationName, "org_id": org.OrganizationId}
+			cypher_query := `CREATE (n:Org {organization:{org_name}, gracenote_organization_id:{org_id}})`
+			params := map[string]interface{}{"org_name": org.OrganizationName, "org_id": org.OrganizationId}
 			ProcessCypher(conn, cypher_query, params)
 
-			cypher_query = `CREATE ({sport_node_name})-[:BELONGS_TO]->({org_node_name})`
-			params = map[string]interface{}{"sport_node_name": sport_node_name, "org_node_name": org_node_name}
-			ProcessCypher(conn, cypher_query, params)
+			_, _ = conn.ExecNeo(`CREATE INDEX ON:Sport(sports_id) CREATE INDEX ON:Org(org_id)`, nil)
+			cypher_query = `MATCH (s:Sport {gracenote_sport_id: {s}})
+					MATCH (o:Org {gracenote_organization_id: {o}})
+					CREATE (o)-[:B]->(s)
+					`
+			result, err := conn.ExecNeo(cypher_query, map[string]interface{}{"s":val.SportsId, "o":org.OrganizationId})
+
+
+			logrus.Info(result)
+			common.Check(err)
 		}
 
 	}
 }
-
 
 func ProcessCypher(conn bolt.Conn, cypher_template string, params map[string]interface{}) {
 	stmt, err := conn.PrepareNeo(cypher_template)
@@ -71,12 +77,13 @@ func ProcessCypher(conn bolt.Conn, cypher_template string, params map[string]int
 
 	result, err := stmt.ExecNeo(params)
 	common.Check(err)
+	logrus.Info(result)
 
-	numResult, err := result.RowsAffected()
+	//numResult, err := result.RowsAffected()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("CREATED ROWS: %d\n", numResult)
+	//fmt.Printf("CREATED ROWS: %d\n", numResult)
 
 	// Closing the statment will also close the rows
 	stmt.Close()
@@ -90,9 +97,9 @@ func GetSport(sportsId string) {
 	common.Check(err)
 
 	params := map[string]string{
-		"api_key": ApiKey,
-		"includeOrg" : "true",
-		"imageSize" : "md",
+		"api_key":    ApiKey,
+		"includeOrg": "true",
+		"imageSize":  "md",
 	}
 
 	common.BuildQuery(req, params)
@@ -111,5 +118,3 @@ func GetSport(sportsId string) {
 	sportsList.SaveSportsList()
 
 }
-
-
