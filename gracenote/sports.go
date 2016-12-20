f5ed349ce94628f2c92fc1a4bf56f51cdaa4f1d6
Nem <nemesisesq@gmail.com>
2016-12-20 00:52:05 -0500

    saving tests

--- gracenote/sports.go
+++ gracenote/sports.go
@@ -25,67 +25,75 @@ type Sport struct {
 
 type SportsList []Sport
 
+type PrefferedImage struct {
+	Width    string `json:"width" bson:"width"`
+	Height   string `json:"height" bson:"height"`
+	Uri      string `json:"uri" bson:"uri"`
+	Category string `json:"category" bson:"category"`
+	Primary  string `json:"primary" bson:"primary"`
+	Tier     string `json:"tier" bson:"tier"`
+}
+type University  struct {
+	UniversityId   string        `json:"universityId" bson:"universityId"`
+	UniversityName string        `json:"universityName" bson:"universityName"`
+	NickName       string         `json:"nickName" bson:"nickName"`
+	PrefferedImage PrefferedImage `json:"prefferedImage" bson:"prefferedImage"`
+}
+
+type Universities []University
+
+func (unis Universities) SaveUniversities() {
+	driver := bolt.NewDriver()
+	conn, err := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	defer conn.Close()
+	common.Check(err)
+	for _, val := range unis {
+		cypher_query := `
+			MERGE (u:University {gracenote_university_id:{uni_id}, name:{uni_name}, nickname:{uni_nick}, img:{uri}})
+		`
+		params := map[string]interface{}{"uni_id": val.UniversityId, "uni_name": val.UniversityName, "uni_nick":val.NickName, "img":val.PrefferedImage.Uri}
+		ProcessCypher(conn, cypher_query, params)
+	}
+
+}
+
 func (sl SportsList) SaveSportsList() {
-
 	logrus.SetFormatter(&logrus.JSONFormatter{})
-
 	driver := bolt.NewDriver()
 	logrus.Info(os.Getenv("NEO4JBOLT"))
-
 	conn, err := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
-
+	defer conn.Close()
 	common.Check(err)
-
-
-
 	for _, val := range sl {
-		//rmvSpace := strings.NewReplacer(" ", "", ".", "_", "'", "", "-", "", "/", "")
-		//sport_node_name := rmvSpace.Replace(val.SportsName)
-		cypher_query := `CREATE (n:Sport {sport_name:{sport_name}, gracenote_sport_id:{sports_id}})`
-
-		params := map[string]interface{}{"sport_name": val.SportsName, "sports_id": val.SportsId}
-
+		cypher_query := `MERGE (s:Sport {sport_name:{sport_name}, gracenote_sport_id:{sports_id}})`
+		params := map[string]interface{}{"sport_name": val.SportsName, "sports_id": val.SportsId, }
 		ProcessCypher(conn, cypher_query, params)
-
 		for _, org := range val.Organizations {
-
-			//org_node_name := rmvSpace.Replace(org.OrganizationName)
-
-			cypher_query := `CREATE (n:Org {organization:{org_name}, gracenote_organization_id:{org_id}})`
-			params := map[string]interface{}{"org_name": org.OrganizationName, "org_id": org.OrganizationId}
+			cypher_query := `MERGE (o:Org {organization:{org_name}, gracenote_organization_id:{org_id}, preffered_image:{img}})`
+			params := map[string]interface{}{"org_name": org.OrganizationName, "org_id": org.OrganizationId, "img":fmt.Sprintf("%v", org.PreferredImage)}
 			ProcessCypher(conn, cypher_query, params)
-
-			_, _ = conn.ExecNeo(`CREATE INDEX ON:Sport(sports_id) CREATE INDEX ON:Org(org_id)`, nil)
 			cypher_query = `MATCH (s:Sport {gracenote_sport_id: {s}})
 					MATCH (o:Org {gracenote_organization_id: {o}})
-					CREATE (o)-[:B]->(s)
+					MERGE (o)-[:REPRESENTS]->(s)
 					`
-			result, err := conn.ExecNeo(cypher_query, map[string]interface{}{"s":val.SportsId, "o":org.OrganizationId})
-
-
-			logrus.Info(result)
-			common.Check(err)
+			params = map[string]interface{}{"s":val.SportsId, "o":org.OrganizationId}
+			ProcessCypher(conn, cypher_query, params)
 		}
-
 	}
 }
 
 func ProcessCypher(conn bolt.Conn, cypher_template string, params map[string]interface{}) {
 	stmt, err := conn.PrepareNeo(cypher_template)
-
 	common.Check(err)
-
 	result, err := stmt.ExecNeo(params)
 	common.Check(err)
 	logrus.Info(result)
-
-	//numResult, err := result.RowsAffected()
-	if err != nil {
-		panic(err)
+	if val, ok := result.RowsAffected(); ok {
+		//numResult, err := result.RowsAffected()
+		common.Check(err)
+		fmt.Printf("CREATED ROWS: %d\n", val)
 	}
-	//fmt.Printf("CREATED ROWS: %d\n", numResult)
-
-	// Closing the statment will also close the rows
+	//Closing the statment will also close the rows
 	stmt.Close()
 }
 
@@ -93,28 +101,37 @@ func GetSport(sportsId string) {
 	sClient := &http.Client{}
 	url := fmt.Sprintf("%v/%v", SportsUri, sportsId)
 	req, err := http.NewRequest("GET", url, nil)
-
 	common.Check(err)
-
 	params := map[string]string{
 		"api_key":    ApiKey,
 		"includeOrg": "true",
 		"imageSize":  "md",
 	}
-
 	common.BuildQuery(req, params)
-
 	res, err := sClient.Do(req)
 	common.Check(err)
-
 	decoder := json.NewDecoder(res.Body)
-
 	sportsList := SportsList{}
-
 	err = decoder.Decode(&sportsList)
-
 	common.Check(err)
-
 	sportsList.SaveSportsList()
+}
 
+func GetUniversities() {
+	sClient := &http.Client{}
+	url := fmt.Sprintf("%v/%v", SportsUri, "universities")
+	req, err := http.NewRequest("GET", url, nil)
+	common.Check(err)
+	params := map[string]string{
+		"api_key":    ApiKey,
+		"imageSize":  "md",
+	}
+	common.BuildQuery(req, params)
+	res, err := sClient.Do(req)
+	common.Check(err)
+	decoder := json.NewDecoder(res.Body)
+	unis := &Universities{}
+	err = decoder.Decode(unis)
+	common.Check(err)
+	unis.SaveUniversities()
 }
--- gracenote/sports_test.go
+++ gracenote/sports_test.go
@@ -6,19 +6,40 @@ import (
 	"os"
 )
 
+func setUp(){
+	os.Setenv("NEO4JBOLT", "bolt://neo4j:prelude@localhost:7687")
+}
+
+func TestMain(m *testing.M) {
+	setUp()
+	retCode := m.Run()
+	//myTeardownFunction()
+	os.Exit(retCode)
+}
+
+
 func TestGestSports(t *testing.T) {
 
-	os.Setenv("NEO4JBOLT", "bolt://neo4j:prelude@localhost:7687")
+	//os.Setenv("NEO4JBOLT", "bolt://neo4j:prelude@localhost:7687")
 	GetSport("all")
 	driver := bolt.NewDriver()
 	conn, _ := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
-
+	defer conn.Close()
 	result, _ := conn.ExecNeo("MATCH (n:Sport) return COUNT(n)", nil)
-
 	rows, _ := result.RowsAffected()
-
 	if rows == 0 {
 		t.Error("Expected Results to be in the database but 0 were found")
 	}
+}
 
+func TestGetUniversities(t *testing.T) {
+	GetUniversities()
+	driver := bolt.NewDriver()
+	conn, _ := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	defer conn.Close()
+	result, _ := conn.ExecNeo("MATCH (n:University) return COUNT(u)", nil)
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		t.Error("Expected Results to be in the database but 0 were found")
+	}
 }
