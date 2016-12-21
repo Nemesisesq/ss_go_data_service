2047e4442e0dbc1a42d99d20aa03061ff0489ba7
Nem <nemesisesq@gmail.com>
2016-12-20 21:04:22 -0500

    created scripts to get all proffessional and college teams available

--- gracenote/sports.go
+++ gracenote/sports.go
@@ -7,14 +7,38 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"github.com/johnnadratowski/golang-neo4j-bolt-driver/structures/graph"
 	"github.com/nemesisesq/ss_data_service/common"
 	"gopkg.in/square/go-jose.v1/json"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type Team struct {
+	TeamBrandId   string `json:"teamBrandId" bson:"team_brand_id"`
+	SportsId      string `json:"sportsId" bson:"sports_id"`
+	TeamBrandName string `json:"teamBrandName" bson:"team_brand_name"`
+	Nickname      string `json:"nickname" bson:"nickname"`
+	ProperName    string `json:"properName" bson:"proper_name"`
+	Abbreviation  string `json:"abbreviation" bson:"abbreviation"`
+}
+
+type CollegeTeam struct {
+	Team
+	University     University `json:"university" bson:"university"`
+	PreferredImage PreferredImage `json:"preferredImage" bson:"preferred_image"`
+	Img            string        `bson:"img"`
+}
+
+type CollegeTeams []CollegeTeam
+
+type Teams []Team
+
 type Organization struct {
-	OrganizationId   string            `json:"organizationId" bson:"organization_id"`
-	OrganizationName string            `json:"organizationName" bson:"organizationName"`
-	PreferredImage   map[string]string `json:"preferredImage" bson:"preferredImage"`
+	OrganizationId   interface{}      `json:"organizationId" bson:"gracenote_organization_id"`
+	OrganizationName string      `json:"organizationName" bson:"organization"`
+	PreferredImage   PreferredImage `json:"preferredImage" bson:"preferred_image"`
+	Img              string        `bson:"img"`
+	Teams            `json:"teams"`
 }
 
 type Sport struct {
@@ -25,7 +49,7 @@ type Sport struct {
 
 type SportsList []Sport
 
-type PrefferedImage struct {
+type PreferredImage struct {
 	Width    string `json:"width" bson:"width"`
 	Height   string `json:"height" bson:"height"`
 	Uri      string `json:"uri" bson:"uri"`
@@ -33,14 +57,96 @@ type PrefferedImage struct {
 	Primary  string `json:"primary" bson:"primary"`
 	Tier     string `json:"tier" bson:"tier"`
 }
-type University  struct {
-	UniversityId   string        `json:"universityId" bson:"universityId"`
-	UniversityName string        `json:"universityName" bson:"universityName"`
+type University struct {
+	UniversityId   string         `json:"universityId" bson:"universityId"`
+	UniversityName string         `json:"universityName" bson:"universityName"`
 	NickName       string         `json:"nickName" bson:"nickName"`
-	PrefferedImage PrefferedImage `json:"prefferedImage" bson:"prefferedImage"`
+	PreferredImage PreferredImage `json:"preferredImage" bson:"preferred_image"`
+	Img            string        `bson:"img"`
 }
 
 type Universities []University
+
+func (ct CollegeTeams) SaveCollegeTeams() {
+	driver := bolt.NewDriver()
+	conn, err := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	defer conn.Close()
+	common.Check(err)
+
+	for _, val := range ct {
+		cypher_query := `
+			MATCH (s:Sport {gracenote_sport_id:{sports_id}})
+			MATCH (u:University {gracenote_university_id: {uni_id}})
+			MERGE (t:Team {team_brand_id:{brand_id}, sports_id:{sports_id}, name:{team_name}, nickname:{nick}, propername:{propername}, abbreviation:{abbr}, img:{uri}})
+			MERGE (t)-[:PLAYS]->(s)
+			MERGE (t)-[:BELONGS_TO]->(u)
+		`
+		params := map[string]interface{}{
+			"brand_id":fmt.Sprint(val.TeamBrandId),
+			"sports_id":fmt.Sprint(val.SportsId),
+			"team_name":val.TeamBrandName,
+			"nick": val.Nickname,
+			"propername": val.ProperName,
+			"abbr": val.Abbreviation,
+			"uni_id": fmt.Sprintf(val.University.UniversityId),
+			"uri": fmt.Sprint(val.PreferredImage.Uri),
+		}
+		ProcessCypher(conn, cypher_query, params)
+
+		//cypher_query = `
+		//	MATCH (t:Team {team_brand_id:{brand_id}})
+		//
+		//	MATCH (o:Org {gracenote_organization_id:{org_id}})
+		//
+		//	MERGE (t)-[:MEMBER_OF]->(o)
+		//`
+		//params = map[string]interface{}{
+		//	"brand_id":fmt.Sprint(val.TeamBrandId),
+		//	"sports_id":fmt.Sprint(val.SportsId),
+		//}
+		//ProcessCypher(conn, cypher_query, params)
+	}
+
+}
+
+func (teams Teams) SaveTeams(org_id float64) {
+	driver := bolt.NewDriver()
+	conn, err := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	defer conn.Close()
+	common.Check(err)
+	//query_slice := []string{}
+	//params_slice := []string{}
+
+	for _, val := range teams {
+		cypher_query := `
+			MERGE (t:Team {team_brand_id:{brand_id}, sports_id:{sports_id}, name:{team_name}, nickname:{nick}, propername:{propername}, abbreviation:{abbr}})
+		`
+		params := map[string]interface{}{
+			"brand_id":fmt.Sprint(val.TeamBrandId),
+			"sports_id":fmt.Sprint(val.SportsId),
+			"team_name":val.TeamBrandName,
+			"nick": val.Nickname,
+			"propername": val.ProperName,
+			"abbr": val.Abbreviation,
+			"org_id": org_id,
+		}
+		ProcessCypher(conn, cypher_query, params)
+
+		cypher_query = `
+			MATCH (t:Team {team_brand_id:{brand_id}})
+			MATCH (s:Sport {gracenote_sport_id:{sports_id}})
+			MATCH (o:Org {gracenote_organization_id:{org_id}})
+			MERGE (t)-[:PLAYS]->(s)
+			MERGE (t)-[:MEMBER_OF]->(o)
+		`
+		params = map[string]interface{}{
+			"brand_id":fmt.Sprint(val.TeamBrandId),
+			"sports_id":fmt.Sprint(val.SportsId),
+			"org_id": fmt.Sprint(org_id),
+		}
+		ProcessCypher(conn, cypher_query, params)
+	}
+}
 
 func (unis Universities) SaveUniversities() {
 	driver := bolt.NewDriver()
@@ -51,11 +157,11 @@ func (unis Universities) SaveUniversities() {
 		cypher_query := `
 			MERGE (u:University {gracenote_university_id:{uni_id}, name:{uni_name}, nickname:{uni_nick}, img:{uri}})
 		`
-		params := map[string]interface{}{"uni_id": val.UniversityId, "uni_name": val.UniversityName, "uni_nick":val.NickName, "img":val.PrefferedImage.Uri}
+		params := map[string]interface{}{"uni_id": val.UniversityId, "uni_name": val.UniversityName, "uni_nick": val.NickName, "uri": fmt.Sprintf("%v", val.PreferredImage.Uri)}
 		ProcessCypher(conn, cypher_query, params)
 	}
-
 }
+
 
 func (sl SportsList) SaveSportsList() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
@@ -63,20 +169,21 @@ func (sl SportsList) SaveSportsList() {
 	logrus.Info(os.Getenv("NEO4JBOLT"))
 	conn, err := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
 	defer conn.Close()
+	//conn.ExecPipeline()
 	common.Check(err)
 	for _, val := range sl {
 		cypher_query := `MERGE (s:Sport {sport_name:{sport_name}, gracenote_sport_id:{sports_id}})`
-		params := map[string]interface{}{"sport_name": val.SportsName, "sports_id": val.SportsId, }
+		params := map[string]interface{}{"sport_name": val.SportsName, "sports_id": val.SportsId}
 		ProcessCypher(conn, cypher_query, params)
 		for _, org := range val.Organizations {
-			cypher_query := `MERGE (o:Org {organization:{org_name}, gracenote_organization_id:{org_id}, preffered_image:{img}})`
-			params := map[string]interface{}{"org_name": org.OrganizationName, "org_id": org.OrganizationId, "img":fmt.Sprintf("%v", org.PreferredImage)}
+			cypher_query := `MERGE (o:Org {organization:{org_name}, gracenote_organization_id:{org_id}, img:{uri}})`
+			params := map[string]interface{}{"org_name": org.OrganizationName, "org_id": org.OrganizationId, "uri": fmt.Sprintf("%v", org.PreferredImage.Uri)}
 			ProcessCypher(conn, cypher_query, params)
 			cypher_query = `MATCH (s:Sport {gracenote_sport_id: {s}})
 					MATCH (o:Org {gracenote_organization_id: {o}})
 					MERGE (o)-[:REPRESENTS]->(s)
 					`
-			params = map[string]interface{}{"s":val.SportsId, "o":org.OrganizationId}
+			params = map[string]interface{}{"s": val.SportsId, "o": org.OrganizationId}
 			ProcessCypher(conn, cypher_query, params)
 		}
 	}
@@ -87,11 +194,15 @@ func ProcessCypher(conn bolt.Conn, cypher_template string, params map[string]int
 	common.Check(err)
 	result, err := stmt.ExecNeo(params)
 	common.Check(err)
-	logrus.Info(result)
-	if val, ok := result.RowsAffected(); ok {
+	logrus.WithFields(logrus.Fields{
+		"Cypher QueryResult": result,
+		"params": params,
+	}).Info()
+	if val, ok := result.RowsAffected(); ok != nil {
 		//numResult, err := result.RowsAffected()
 		common.Check(err)
-		fmt.Printf("CREATED ROWS: %d\n", val)
+		logrus.Info("CREATED ROWS", val)
+
 	}
 	//Closing the statment will also close the rows
 	stmt.Close()
@@ -123,8 +234,8 @@ func GetUniversities() {
 	req, err := http.NewRequest("GET", url, nil)
 	common.Check(err)
 	params := map[string]string{
-		"api_key":    ApiKey,
-		"imageSize":  "md",
+		"api_key":   ApiKey,
+		"imageSize": "md",
 	}
 	common.BuildQuery(req, params)
 	res, err := sClient.Do(req)
@@ -134,4 +245,72 @@ func GetUniversities() {
 	err = decoder.Decode(unis)
 	common.Check(err)
 	unis.SaveUniversities()
+}
+
+func GetTeamsInAnOrganization() {
+	driver := bolt.NewDriver()
+	logrus.Info(os.Getenv("NEO4JBOLT"))
+	conn, err := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	common.Check(err)
+	defer conn.Close()
+
+	data, rowMetaData, _, _ := conn.QueryNeoAll(`MATCH (n:Org) RETURN n`, nil)
+
+	orgs := []Organization{}
+
+	logrus.Info(rowMetaData)
+	for _, val := range data {
+		org := &Organization{}
+		the_bson, err := bson.Marshal(val[0].(graph.Node).Properties)
+		err = bson.Unmarshal(the_bson, &org)
+		common.Check(err)
+		orgs = append(orgs, *org)
+	}
+
+	for _, val := range orgs {
+		sClient := http.Client{}
+		url := fmt.Sprintf("%v/%v/%v", SportsUri, "organizations", val.OrganizationId)
+		req, err := http.NewRequest("GET", url, nil)
+		common.Check(err)
+		params := map[string]string{
+			"api_key":   ApiKey,
+			"imageSize": "Md",
+		}
+		common.BuildQuery(req, params)
+		res, err := sClient.Do(req)
+
+		common.Check(err)
+		if res.StatusCode == 200 {
+
+			decoder := json.NewDecoder(res.Body)
+			logrus.Info(res.Body)
+
+			orgs := []Organization{}
+
+			if err := decoder.Decode(&orgs); err == nil {
+				//logrus.Info(orgs)
+				common.Check(err)
+				org := orgs[0]
+				org.Teams.SaveTeams(org.OrganizationId.(float64))
+			}
+		}
+	}
+}
+func GetTeamsAtAUniversity(uni_id string) {
+	sClient := &http.Client{}
+	url := fmt.Sprintf("%v/%v/%v", SportsUri, "universities", uni_id)
+	req, err := http.NewRequest("GET", url, nil)
+	common.Check(err)
+	params := map[string]string{
+		"includeTeam": "true",
+		"api_key":   ApiKey,
+	}
+	common.BuildQuery(req, params)
+	res, err := sClient.Do(req)
+	common.Check(err)
+	decoder := json.NewDecoder(res.Body)
+	college_teams := &CollegeTeams{}
+	err = decoder.Decode(&college_teams)
+	common.Check(err)
+	college_teams.SaveCollegeTeams()
 }
--- gracenote/sports_test.go
+++ gracenote/sports_test.go
@@ -22,8 +22,8 @@ func TestGestSports(t *testing.T) {
 
 	//os.Setenv("NEO4JBOLT", "bolt://neo4j:prelude@localhost:7687")
 	GetSport("all")
-	driver := bolt.NewDriver()
-	conn, _ := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	bolt_driver := bolt.NewDriver()
+	conn, _ := bolt_driver.OpenNeo(os.Getenv("NEO4JBOLT"))
 	defer conn.Close()
 	result, _ := conn.ExecNeo("MATCH (n:Sport) return COUNT(n)", nil)
 	rows, _ := result.RowsAffected()
@@ -34,10 +34,33 @@ func TestGestSports(t *testing.T) {
 
 func TestGetUniversities(t *testing.T) {
 	GetUniversities()
-	driver := bolt.NewDriver()
-	conn, _ := driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	bolt_driver := bolt.NewDriver()
+	conn, _ := bolt_driver.OpenNeo(os.Getenv("NEO4JBOLT"))
 	defer conn.Close()
-	result, _ := conn.ExecNeo("MATCH (n:University) return COUNT(u)", nil)
+	result, _ := conn.ExecNeo("MATCH (n:University) return COUNT(n)", nil)
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		t.Error("Expected Results to be in the database but 0 were found")
+	}
+}
+
+func TestGetTeamsInAnOrganization(t *testing.T) {
+	GetTeamsInAnOrganization()
+	bolt_driver := bolt.NewDriver()
+	conn, _ := bolt_driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	defer conn.Close()
+	result, _ := conn.ExecNeo("MATCH (n:University) return COUNT(n)", nil)
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		t.Error("Expected Results to be in the database but 0 were found")
+	}
+}
+func TestGetTeamsAtAUniversity(t *testing.T) {
+	GetTeamsAtAUniversity("all")
+	bolt_driver := bolt.NewDriver()
+	conn, _ := bolt_driver.OpenNeo(os.Getenv("NEO4JBOLT"))
+	defer conn.Close()
+	result, _ := conn.ExecNeo("MATCH (n:University) return COUNT(n)", nil)
 	rows, _ := result.RowsAffected()
 	if rows == 0 {
 		t.Error("Expected Results to be in the database but 0 were found")
