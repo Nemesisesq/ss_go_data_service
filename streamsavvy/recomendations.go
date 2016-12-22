1f29768ba9bb61dfac5110733bb28ca5bd968d25
Nem <nemesisesq@gmail.com>
2016-12-22 02:19:32 -0500

    improved reliability of recomendations

--- streamsavvy/recomendations.go
+++ streamsavvy/recomendations.go
@@ -1,15 +1,17 @@
 package streamsavvy
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"sync"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/websocket"
 	"github.com/nemesisesq/ss_data_service/common"
 	"github.com/nemesisesq/ss_data_service/middleware"
 	"github.com/streadway/amqp"
 	"gopkg.in/redis.v5"
-	"net/http"
-	"sync"
 )
 
 type Reco struct {
@@ -18,7 +20,16 @@ type Reco struct {
 	sock       *websocket.Conn
 }
 
+
 func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
+
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	rmqc := r.Context().Value("rabbitmq").(middleware.RMQCH)
@@ -26,7 +37,7 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 	cleanup := r.Context().Value("cleanup").(chan string)
 
 	SimKey := "ss_reco:%v:%v"
-	categories := []string{"genres", "tags", "cast"}
+	categories := []string{"genres", "cast"}
 
 	common.Check(err)
 	if err != nil {
@@ -55,7 +66,7 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 
 			q := fmt.Sprintf(SimKey, cat, reco.GuideboxId)
 
-			res := r_client.ZRange(q, 0, 3)
+			res := r_client.ZRange(q, 0, 5)
 
 			reco_ids = append(reco_ids, res.Val()...)
 			//common.Check(err)
@@ -64,10 +75,7 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 
 		RemoveDuplicates(&reco_ids)
 
-		for _, val := range reco_ids {
-
-			reco.PublishShowInfo(val, rmqc)
-		}
+		reco.PublishShowInfo(reco_ids, rmqc)
 
 		//TODO use this to update show recomendations later.
 		//reco.PublishShowInfo(p, rmqc)
@@ -90,12 +98,12 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 			logrus.Info("Listening to recomendation results channel")
 			msgs, err := rmqc.RX.Consume(
 				rx_q.Name, // queue
-				"", // consumer
-				true, // auto-ack
-				false, // exclusive
-				false, // no-local
-				false, // no-wait
-				nil, // args
+				"",        // consumer
+				true,      // auto-ack
+				false,     // exclusive
+				false,     // no-local
+				false,     // no-wait
+				nil,       // args
 			)
 			common.Check(err)
 			if err != nil {
@@ -111,7 +119,7 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 				case m := <-msgs:
 
 					if len(m.Body) > 2 {
-
+						//logrus.Info(m.Body)
 						err = reco.send(messageType, m.Body)
 						common.Check(err)
 						if err != nil {
@@ -123,6 +131,8 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 				case <-cleanup:
 					rmqc.RX.Close()
 					break
+
+				default:
 				}
 			}
 
@@ -130,12 +140,12 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 
 		}()
 
-		select {
-		case <-cleanup:
-			rmqc.RX.Close()
-			rmqc.TX.Close()
-			return
-		}
+		//select {
+		//case <-cleanup:
+		//	rmqc.RX.Close()
+		//	rmqc.TX.Close()
+		//	return
+		//}
 	}
 }
 
@@ -144,6 +154,7 @@ func HandleRecomendations(w http.ResponseWriter, r *http.Request) {
 func (r Reco) send(msg int, m []byte) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
+	defer logrus.Info("Unlocking mutex")
 	return r.sock.WriteMessage(msg, m)
 }
 
@@ -154,9 +165,9 @@ func (r Reco) sendJson(msg int, m interface{}) error {
 }
 
 //TODO it would be nice to access database straight from  go
-func (r Reco) PublishShowInfo(show_id string, rmqc middleware.RMQCH) {
+func (r Reco) PublishShowInfo(show_ids []string, rmqc middleware.RMQCH) {
 
-	logrus.WithField("connection address", r.sock).Info("publishing", show_id)
+	logrus.WithField("connection address", r.sock).Info("publishing", show_ids)
 	tx_q, err := rmqc.TX.QueueDeclare(
 		"reco_engine",
 		false,
@@ -172,14 +183,18 @@ func (r Reco) PublishShowInfo(show_id string, rmqc middleware.RMQCH) {
 		rmqc.TX.Close()
 	}
 
+	//b_showIds := StringSliceToByteSlice(show_ids)
+
+	the_json, err := json.Marshal(show_ids)
+	common.Check(err)
 	err = rmqc.TX.Publish(
-		"", // exchange
+		"",        // exchange
 		tx_q.Name, // routing key
-		false, // mandatory
-		false, // immediate
+		false,     // mandatory
+		false,     // immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
-			Body:        []byte(show_id),
+			Body:        the_json,
 		})
 	common.Check(err)
 	if err != nil {
@@ -187,6 +202,7 @@ func (r Reco) PublishShowInfo(show_id string, rmqc middleware.RMQCH) {
 		rmqc.TX.Close()
 	}
 }
+
 
 func RemoveDuplicates(xs *[]string) {
 	found := make(map[string]bool)
